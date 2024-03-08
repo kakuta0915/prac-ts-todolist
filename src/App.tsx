14fd883513ba3fd228ab70eeae6f14d03e60772f
Takeru Kakuta <kakuta19960915@gmail.com>
2024-03-08 19:12:05 +0900

    ✨ feat: タスク機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // 入力した文字列を格納する配列
+  const [inputValue, setInputValue] = useState("");
+
+  // 空の配列にTodoを宣言
+  const [todos, setTodos]= useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    // TodosにnewTodoをスプレット構文で格納
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  }
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
+  
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with TypeScript</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className="inputText" />
           <input type="submit" value="作成" className="submitButton" />
         </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input 
+                type="text" 
+                onChange={(e) => handleEdit(todo.id, e.target.value)} 
+                className="inputText" 
+                value={todo.inputValue} 
+                disabled={todo.checked}
+              />
+              <input 
+                type="checkbox" 
+                onChange={(e) => handleChecked(todo.id, todo.checked)} 
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
