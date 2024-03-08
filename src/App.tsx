75d619aa64d50e30b2b91e9a99d59ae254049fb7
Takeru Kakuta <kakuta19960915@gmail.com>
2024-03-08 17:22:40 +0900

    ✨ feat: タイトルと検索ボックス追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <h2>Todoリスト with TypeScript</h2>
+        <form onSubmit={() => {}}>
+          <input type="text" onChange={() => {}} className="inputText" />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+      </div>
     </div>
   );
 }
